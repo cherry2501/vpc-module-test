ae24f3e2a6befd76e4c20b1eaa73b0481a343182
cherry2501 <saicharangudiya@gmail.com>
2025-01-21 17:27:51 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "vpc_cidr" {
+  default = "13.12.0.0/16"
+}
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+variable "public_subnet_cidrs" {
+  default = ["13.12.1.0/24","13.12.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["13.12.11.0/24","13.12.12.0/24"]
+}
+
+variable "databse_subnet_cidrs" {
+  default = ["13.12.21.0/24","13.12.22.0/24"]
+}
---
+++ vpc.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+
+    source = "../terraform-module-vpc"
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    common_tags = var.common_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.databse_subnet_cidrs
+
+}
